c2c6eb88293f17e4a0fa35f842d3a9bc33040d21
David Plappert <david.plappert@ideacrew.com>
2016-08-19 20:19:31 -0500

    renaming plugin to redmine_reports

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  unloadable
-
-
-end
--- init.rb
+++ init.rb
@@ -1,34 +1,12 @@
-Redmine::Plugin.register :redmine_sla_dashboard do
+Redmine::Plugin.register :redmine_reports do
   name 'Redmine Reports plugin'
   author 'IdeaCrew, Inc'
-  description 'Do you have any issues that are outside your SLA windows? Do you have any close to being outside your SLA?'
+  description ''
   version '0.0.1'
-  url 'https://github.com/dchbx/redmine_report'
+  url 'https://github.com/dchbx/redmine_reports'
   author_url 'http://ideacrew.com'
-  menu :top_menu, :reports, { :controller => 'home', :action => 'index' }, :caption => 'Reports'
+  menu :top_menu, :reports, { :controller => 'reports', :action => 'sla' }, :caption => 'Reports'
   settings :default => {
-    :SLAs => [
-      {
-        :type => "tracker",
-        :name => "Hot Fix",
-        :secToComplete => 2*24*60*60
-      },
-      {
-        :type => "priority",
-        :name => "Immediate",
-        :secToComplete => 2*24*60*60
-      },
-      {
-        :type => "priority",
-        :name => "Urgent",
-        :secToComplete => 5*24*60*60,
-      },
-      {
-        :type => "priority",
-        :name => "High",
-        :secToComplete => 10*24*60*60,
-      },
-    }
   }, :partial => 'settings/report_settings'
   requires_redmine :version_or_higher => '3.0'
 end
