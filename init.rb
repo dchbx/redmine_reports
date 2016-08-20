5c73502a2f8303ee14e121bb6f7d0104a65b7855
David Plappert <david.plappert@ideacrew.com>
2016-08-19 20:10:25 -0500

    renaming plugin to redmine_reports

--- init.rb
+++ init.rb
@@ -1,12 +1,34 @@
 Redmine::Plugin.register :redmine_sla_dashboard do
-  name 'Redmine Sla Dashboard plugin'
+  name 'Redmine Reports plugin'
   author 'IdeaCrew, Inc'
   description 'Do you have any issues that are outside your SLA windows? Do you have any close to being outside your SLA?'
   version '0.0.1'
-  url 'https://github.com/dchbx/redmine_sla_dashboard'
+  url 'https://github.com/dchbx/redmine_report'
   author_url 'http://ideacrew.com'
-  menu :top_menu, :sla_dashboard, { :controller => 'home', :action => 'index' }, :caption => 'SLA Dash'
+  menu :top_menu, :reports, { :controller => 'home', :action => 'index' }, :caption => 'Reports'
   settings :default => {
-  }, :partial => 'settings/sla_settings'
+    :SLAs => [
+      {
+        :type => "tracker",
+        :name => "Hot Fix",
+        :secToComplete => 2*24*60*60
+      },
+      {
+        :type => "priority",
+        :name => "Immediate",
+        :secToComplete => 2*24*60*60
+      },
+      {
+        :type => "priority",
+        :name => "Urgent",
+        :secToComplete => 5*24*60*60,
+      },
+      {
+        :type => "priority",
+        :name => "High",
+        :secToComplete => 10*24*60*60,
+      },
+    }
+  }, :partial => 'settings/report_settings'
   requires_redmine :version_or_higher => '3.0'
 end
